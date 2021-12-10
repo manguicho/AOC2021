f69ad725e299e8239bd689594eed1c9de04c3bcd
Sebastian Mangisch <manguicho@gmail.com>
2021-12-10 15:01:23 -0300

    Update day10.js

--- day10.js
+++ day10.js
@@ -126,12 +126,11 @@ let pairs = [
 {open:"[" , close:"]" , cost:57   , cost2:2}, 
 {open:"{" , close:"}" , cost:1197 , cost2:3}, 
 {open:"<" , close:">" , cost:25137, cost2:4}]
-let opens = pairs.map(e=>e.open)
 
 const check = (str)=>{
 	let last_open = []
 	for (o of str){
-		if (opens.includes(o)) last_open.push(o)
+		if (pairs.map(e=>e.open).includes(o)) last_open.push(o)
 		else if (pairs.filter(e=>e.close==o && last_open[last_open.length-1]==e.open).length>0) last_open.pop()
 		else return [pairs.filter(e=>e.close==o).map(e=>e.cost)[0],complete(last_open.join(""))]
 	}
@@ -144,11 +143,10 @@ const complete = (str)=>{
 		res*=5
 		res+=Number(pairs.filter(e=>e.open==str[i]).map(e=>e.cost2))
 	}
-	return Number(res)
+	return res
 }
 
 
 console.log("P1", s.map(e=>check(e)[0]).reduce((a,b)=>a+b))
-
 s = s.filter(e=>check(e)[0]==0).map(e=>check(e)[1]).sort((a,b)=>a-b)
 console.log("P2", s[Math.floor(s.length/2)])
