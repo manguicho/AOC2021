4495428f7c9071348159bfeb2f0f5099d7588d1f
Sebastian Mangisch <manguicho@gmail.com>
2021-12-12 12:26:20 -0300

    Add files via upload

---
+++ day11.js
@@ -0,0 +1,73 @@
+let s= `6318185732
+1122687135
+5173237676
+8754362612
+5718474666
+8443654137
+1247634346
+1446514585
+6717288267
+1727871228`.split('\n')
+    .map(e => e.split('').map(e => parseInt(e)));
+
+ss=`11111
+19991
+19191
+19991
+11111`.split('\n') //EXAMPLE
+    .map(e => e.split('').map(e => parseInt(e)));
+
+const l=(...x)=>{console.log(...x);return x[0]}
+function sum(...x){if(Array.isArray(x[0])) return x[0].reduce((a,b)=>a+b,0); else return x.reduce((a,b)>a+b,0);}
+
+const nearby = (y,x) =>
+  [
+    [(y), (x) - 1],
+    [(y), (x) + 1],
+    [(y) - 1, (x)],
+    [(y) + 1, (x)],
+    [(y) - 1, (x)+1],
+    [(y) + 1, (x)-1],
+    [(y) - 1, (x)-1],
+    [(y) + 1, (x)+1],
+  ].filter(([yy, xx]) => yy >= 0 && yy < s.length && xx >= 0 && xx < s[0].length);
+
+
+const flash = (input, y,x)=>{
+	flashed.push([y,x])
+	let neighbours = nearby(y,x)
+	for (n of neighbours){		
+		let num = 0; num = parseInt(input[n[0]][n[1]])		
+		if (!flashed.map(e=>e.join("")).includes(n.join("")) && num<10 && num > 0){
+			input[n[0]][n[1]] = num+1; //l(y,x,n,s[n[0]][n[1]])
+			if (input[n[0]][n[1]] == 10) 	flash(input, n[0],n[1])
+		} 
+	}
+	input[y][x]=0
+}
+
+const solve= (input, steps,p2=false)=>
+{
+	let total = []
+	for (i=0;parseInt(i) < ((p2 == false)?parseInt(steps):Infinity);i++){
+		input = input.map(e=>e.map(f=>f+1))			
+		flashed = []
+		for (let y in input)
+			for (let x in input){
+				if (input[y][x] == 10) {flash(input, parseInt(y),parseInt(x))}
+			}	
+		total.push(flashed.length)
+		if (input.filter(e=>e.join("") == "0000000000").length==10) return parseInt(i)+1
+	}
+	return sum(total)
+}
+
+console.log("P1", solve(s,100,false))  
+console.log("P2", solve(s,Infinity,true))  
+
+
+
+
+
+
+
